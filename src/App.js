4b2e1895ca245e02a219db1c67cfa95bf41bb233
Adrian Casas <adrian.casas.fernandez@gmail.com>
2021-02-03 18:36:37 +0000

    clients APIquery

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css';
 
+//components
+import Client from "./components/client"
+
 const App = () => {
 
   return (
     <div className="App">
-
+      <Client/>
     </div>
   );
 }
--- src/components/client.js
+++ src/components/client.js
@@ -1,1 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Client = () => {
+  const [url, setUrl] = useState("https://cm-app-back.herokuapp.com/client/all");
+  const [responseAPI, setResponseAPI] = useState({ response: "OK" });
+
+  useEffect(() => {
+    const queryAPI = async () => {
+      const query = await axios({ url });
+
+      setResponseAPI(query);
+    };
+
+    queryAPI();
+  }, []);
+
+  const ShowResponse = () => {
+    return Object.keys(responseAPI).map((key) => {
+      return (
+        <div key={key}>
+          {key}: {JSON.stringify(responseAPI[key])}
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <ShowResponse />
+    </div>
+  );
+};
+
+export default Client;
